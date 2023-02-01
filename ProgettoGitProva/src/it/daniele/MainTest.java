aac9188b53a419a8291fe3c801eeb88194209aa3
'DanieleDenza' <danieledenza0@gmail.com>
2023-02-01 14:56:41 +0100

    commit main test

---
+++ ProgettoGitProva/src/it/daniele/MainTest.java
@@ -0,0 +1,13 @@
+package it.daniele;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+		
+		
+	}
+
+}
